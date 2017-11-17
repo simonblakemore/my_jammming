618b9aa961b3169385d0b19ca52c412d06c68c21
Simon <simonblakemore@ymail.com>
2017-11-17 13:04:48 +0000

    cosmetic changes
    changes to match example given

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -28,7 +28,7 @@ class SearchBar extends Component {
     return (
       <div className='SearchBar'>
         <input
-          placeholder="Enter A Song Title"
+          placeholder="Enter A Song, Album or Artist"
           onChange={this.handleTermChange}
         />
         <a onClick={this.search}>SEARCH</a>
